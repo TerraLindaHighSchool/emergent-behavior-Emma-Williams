8a7bbb2f2371be166860d9a42a65fbafa23d3e52
Emma-Williams <60672555+Emma-Williams@users.noreply.github.com>
2020-05-09 20:24:16 -0700

    Emergent Behavior Lab
    Task 1-4 completed.

--- ants/Ant.java
+++ ants/Ant.java
@@ -11,6 +11,8 @@ public class Ant extends Creature
     private boolean carryingFood = false;
     private GreenfootImage image1;
     private GreenfootImage image2;
+    private int MAX_PH_AVAILABLE = 16;
+    private int TIME_FOLLOWING_TRAIL = 30;
     /**
      * Create an ant with a given home hill. The initial speed is zero (not moving).
      */
@@ -19,6 +21,8 @@ public class Ant extends Creature
         setHomeHill(home);
         image1 = getImage();
         image2 = new GreenfootImage("ant-with-food.gif");
+        int phAvailable = MAX_PH_AVAILABLE;
+        int followTrialTimeRemaining = 0;
     }
 
     /**
@@ -55,8 +59,17 @@ public class Ant extends Creature
      
     private void searchForFood()
     {
-        randomWalk();
-        checkForFood();
+        if(followTrailTimeRemaining == 0)
+       { 
+           walkTowardsPheromoneCenter(); // if ants smells a pheromone, walk toward center of the pheromone droplet
+           randomWalk(); // otherwise walk around randomly
+       }  
+        else
+       {
+           followTrailTimeRemaining--; // decrement time remaining
+           walkAwayFromHome();
+       }
+       checkForFood();
     }
 
     private void status()
@@ -64,6 +77,7 @@ public class Ant extends Creature
         if (carryingFood)
         {
          walkTowardsHome();
+         handlePheromoneDrop();
         if (atHome())
           {
             setImage(image1);
@@ -75,5 +89,19 @@ public class Ant extends Creature
         {
             searchForFood();
         }
+    }
+    
+    private void handlePheromoneDrop()
+    {    
+    }
+    
+    public boolean smellPheromone()
+    {
+        
+    }
+    
+    private void walkTowardsPheromoneCenter()
+    {
+        
     }
 }
